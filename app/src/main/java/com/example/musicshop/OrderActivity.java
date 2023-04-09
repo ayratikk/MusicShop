60abc6be79efe9fd2742cf72c5df2af56a5b94e9
Айрат <ayratnasretdinov@gmail.com>
2023-04-09 18:00:24 +0300

    add order

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -5,11 +5,15 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
-
+    String[] addresses = {"ayratnasretdinov@gmail.com"};
+    String subject = "Order from Music Shop";
+    String emailText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +27,26 @@ public class OrderActivity extends AppCompatActivity {
         int quantity = receivedOrderIntent.getIntExtra("quantity", 0);
         double price = receivedOrderIntent.getDoubleExtra("price", 0);
         double orderPrice = receivedOrderIntent.getDoubleExtra("orderPrice", 0);
-
-        TextView orderTextView = findViewById(R.id.orderTextView);
-        orderTextView.setText("Customer name: " + userName + "\n" +
+        emailText = "Customer name: " + userName + "\n" +
                 "Goods name: " + goodsName + "\n" +
                 "Quantity: " + quantity + "\n" +
                 "Price: " +price + "\n" +
-                "Order price: " +orderPrice);
+                "Order price: " +orderPrice;
+
+        TextView orderTextView = findViewById(R.id.orderTextView);
+        orderTextView.setText(emailText);
     }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
 }
